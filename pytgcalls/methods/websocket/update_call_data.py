f7b1ce156a0661ef2aa8c87f3b4ed6f00657f234
Roj Serbest <rojserbest@gmail.com>
2021-03-10 18:57:35 +0300

    Fix mypy errors

--- pytgcalls/methods/websocket/update_call_data.py
+++ pytgcalls/methods/websocket/update_call_data.py
@@ -14,15 +14,15 @@ class UpdateCallData:
         if isinstance(params, str):
             params = json.loads(params)
         if params['result'] == 'PAUSED_AUDIO_STREAM':
-            self._set_status(params['chat_id'], 'paused')
+            self.pytgcalls._set_status(params['chat_id'], 'paused')
         elif params['result'] == 'RESUMED_AUDIO_STREAM':
-            self._set_status(params['chat_id'], 'playing')
+            self.pytgcalls._set_status(params['chat_id'], 'playing')
         elif params['result'] == 'JOINED_VOICE_CHAT':
-            self._add_active_call(params['chat_id'])
-            self._add_call(params['chat_id'])
+            self.pytgcalls._add_active_call(params['chat_id'])
+            self.pytgcalls._add_call(params['chat_id'])
         elif params['result'] == 'LEAVED_VOICE_CHAT':
-            self._rm_active_call(params['chat_id'])
-            self._rm_call(params['chat_id'])
+            self.pytgcalls._rm_active_call(params['chat_id'])
+            self.pytgcalls._rm_call(params['chat_id'])
         for event in self.pytgcalls._on_event_update[
             'EVENT_UPDATE_HANDLER'
         ]:
