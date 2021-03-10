942977522161059ad5457fd69e359cc5dc3629ec
Roj Serbest <rojserbest@gmail.com>
2021-03-10 19:21:51 +0300

    Fix mypy errors

--- pytgcalls/methods/core/__init__.py
+++ pytgcalls/methods/core/__init__.py
@@ -1,6 +1,6 @@
+from .active_calls import ActiveCalls
 from .b_colors import BColors
 from .generate_session import GenerateSession
-from .active_calls import ActiveCalls
 from .get_port_server import GetPortServer
 from .get_user_id import GetUserId
 from .run_js import RunJS
