2a4e9e544fb069012bcebbb5cd55d9a9d3dbcc8a
Laky64 <iraci.matteo@gmail.com>
2021-03-17 19:54:00 +0100

    Pre-commit fix

--- pytgcalls/__init__.py
+++ pytgcalls/__init__.py
@@ -1,4 +1,4 @@
 from .__version__ import __version__  # noqa: F401
 from .methods.logs.py_logs import PyLogs  # noqa: F401
-from .pytgcalls import PyTgCalls  # noqa: F401
 from .methods.stream.stream_type import StreamType  # noqa: F401
+from .pytgcalls import PyTgCalls  # noqa: F401
--- pytgcalls/methods/stream/stream_type.py
+++ pytgcalls/methods/stream/stream_type.py
@@ -2,16 +2,11 @@ class StreamType:
     def __init__(self):
         self._stream_type = 0
 
-
-    # This set seconds buffer to max 3 seconds to load in cache
-    # But this make cpu consumption more heavy
     @property
     def live_stream(self):
         self._stream_type = 3
         return self
 
-    # This set seconds buffer to max 10 seconds to load in cache
-    # But this make not adaptable to live stream or ffmpeg live conversion
     @property
     def local_stream(self):
         self._stream_type = 10
