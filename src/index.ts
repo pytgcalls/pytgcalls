c081ba5de123b132618f2b3f148521dc1fd8a346
Matteo Iraci <iraci.matteo@gmail.com>
2021-05-07 22:31:09 +0200

    Added port message before start

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ import sendUpdate from './send-update';
     const port = parseInt(process.argv[2].split('=')[1]);
     const logMode = parseInt(process.argv[3].split('=')[1]);
     let socket = connect(`ws://localhost:${port}`);
+    console.log('Starting on port: ' + port);
     socket.on('connect', () =>
-        console.log('\x1b[32m', 'Node.js core started!', '\x1b[0m')
+        console.log('\x1b[32m', 'Started NodeJS Core!', '\x1b[0m')
     );
 
     let connections: Array<RTCConnection> = [];
