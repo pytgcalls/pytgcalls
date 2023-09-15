f4c05a9a2b58c0a0e17009ef031cc286dc295e33
Turi <salvatore.toscano1406@gmail.com>
2023-09-15 21:27:47 +0200

    Minor fixes

--- pytgcalls/methods/groups/__init__.py
+++ pytgcalls/methods/groups/__init__.py
@@ -1,15 +1,11 @@
-from .calls import Calls
 from .change_volume_call import ChangeVolumeCall
-from .get_call import GetCall
 from .get_participants import GetParticipants
 from .join_group_call import JoinGroupCall
 from .leave_group_call import LeaveGroupCall
 
 
 class Groups(
-    Calls,
     ChangeVolumeCall,
-    GetCall,
     GetParticipants,
     JoinGroupCall,
     LeaveGroupCall,
--- pytgcalls/methods/groups/calls.py
+++
@@ -1,7 +0,0 @@
-from ...scaffold import Scaffold
-
-
-class Calls(Scaffold):
-    @property
-    def calls(self):
-        return self._call_holder.calls
