466bd0144d268bb3529ccfb486795e5d806260aa
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-07 21:02:59 +0530

    Update leave_group_call.py

--- pytgcalls/methods/groups/leave_group_call.py
+++ pytgcalls/methods/groups/leave_group_call.py
@@ -10,33 +10,41 @@ from ...scaffold import Scaffold
 from ...to_async import ToAsync
 from ...types import LeftVoiceChat
 
-
 class LeaveGroupCall(Scaffold):
     async def leave_group_call(
         self,
         chat_id: Union[int, str],
     ):
+        """
+        Leave a group call.
+
+        :param chat_id: Chat ID of the group call.
+        :raises NoActiveGroupCall: If there is no active group call.
+        :raises ClientNotStarted: If the client is not started.
+        :raises NoMTProtoClientSet: If no MTProto client is set.
+        :raises NotInGroupCallError: If the user is not in the group call.
+        """
         if self._app is not None:
             if self._is_running:
                 chat_id = await self._resolve_chat_id(chat_id)
-                chat_call = await self._app.get_full_chat(
-                    chat_id,
-                )
+                chat_call = await self._app.get_full_chat(chat_id)
 
                 if chat_call is not None:
-                    await self._app.leave_group_call(
-                        chat_id,
-                    )
+                    try:
+                        # Check if the user is in the group call before attempting to leave
+                        await self._app.check_group_call(chat_id)
+                    except NotInGroupCallError:
+                        raise NotInGroupCallError()
+
+                    await self._app.leave_group_call(chat_id)
 
                     try:
-                        await ToAsync(
-                            self._binding.stop,
-                            chat_id,
-                        )
+                        await ToAsync(self._binding.stop, chat_id)
                     except ConnectionError:
                         raise NotInGroupCallError()
 
-                    del self._need_unmute[chat_id]
+                    if chat_id in self._need_unmute:
+                        del self._need_unmute[chat_id]
 
                     await self._on_event_update.propagate(
                         'RAW_UPDATE_HANDLER',
