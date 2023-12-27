d3a9666f179283b18434393486ad51d9711b5072
Laky64 <iraci.matteo@gmail.com>
2023-12-27 15:48:55 +0100

    Fixed codec dection

--- pytgcalls/ffmpeg.py
+++ pytgcalls/ffmpeg.py
@@ -68,8 +68,8 @@ async def check_stream(
             codec_type = stream.get('codec_type', '')
             codec_name = stream.get('codec_name', '')
             image_codecs = ['png', 'jpeg', 'jpg', 'mjpeg']
-            is_image = codec_name in image_codecs
             if codec_type == 'video':
+                is_image = codec_name in image_codecs
                 have_video = True
                 original_width = int(stream.get('width', 0))
                 original_height = int(stream.get('height', 0))
