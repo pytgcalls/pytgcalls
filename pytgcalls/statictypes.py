81d783a60c48f63205a360b40b25245da2fd0bcb
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2024-02-05 18:16:56 +0530

    Update statictypes.py

--- pytgcalls/statictypes.py
+++ pytgcalls/statictypes.py
@@ -3,7 +3,6 @@ from inspect import signature
 from typing import Any
 from typing import Union
 
-
 def statictypes(func):
     sig = signature(func)
 
@@ -14,64 +13,26 @@ def statictypes(func):
         elif origin in (list, set, tuple):
             if obj:
                 return all(is_instance(x, typ.__args__[0]) for x in obj)
-            return (
-                isinstance(obj, list) or
-                isinstance(obj, set) or
-                isinstance(obj, tuple)
-            )
+            return isinstance(obj, (list, set, tuple))
         elif origin is dict:
             if obj:
                 return all(
-                    is_instance(
-                        x,
-                        typ.__args__[0],
-                    ) and is_instance(
-                        obj[x],
-                        typ.__args__[1],
-                    ) for x in obj
+                    is_instance(x, typ.__args__[0]) and is_instance(obj[x], typ.__args__[1]) for x in obj
                 )
             return isinstance(obj, dict)
         return isinstance(obj, typ)
 
     def type_to_string(t):
-        d = None
         if not hasattr(t, '__name__'):
-            d = t
             t = type(t)
         origin = getattr(t, '__origin__', None)
         if origin is Union:
             args = t.__args__
-            if len(args) == 2 and isinstance(None, args[1]):
+            if len(args) == 2 and type(None) in args:
                 return f'Optional[{type_to_string(args[0])}]'
-            return 'Union[' + ', '.join(
-                type_to_string(tt) for tt in args
-            ) + ']'
+            return 'Union[' + ', '.join(type_to_string(tt) for tt in args) + ']'
         elif origin in {list, dict, set, tuple}:
-            return (
-                t.__origin__.__name__.capitalize() + '['
-                + ', '.join(type_to_string(tt) for tt in t.__args__) + ']'
-            )
-
-        if not d and t in {list, dict, set, tuple}:
-            return t.__name__.capitalize()
-        if t in {list, set, tuple}:
-            inner_type = {type_to_string(k) for k in d}
-            if not inner_type:
-                return f'{t.__name__.capitalize()}'
-            inner_type = 'Any' if len(
-                inner_type,
-            ) > 1 else list(inner_type)[0]
-            return f"{t.__name__.capitalize()}[{inner_type}]"
-        elif t is dict:
-            key_type = {type_to_string(k) for k in d.keys()}
-            value_type = {type_to_string(v) for v in d.values()}
-            if not key_type or not value_type:
-                return f'{t.__name__.capitalize()}'
-            key_type = 'Any' if len(key_type) > 1 else list(key_type)[0]
-            value_type = 'Any' if len(
-                value_type,
-            ) > 1 else list(value_type)[0]
-            return f"{t.__name__.capitalize()}[{key_type}, {value_type}]"
+            return f'{t.__origin__.__name__.capitalize()}[{", ".join(type_to_string(tt) for tt in t.__args__)}]'
         return t.__name__
 
     @wraps(func)
@@ -87,18 +48,15 @@ def statictypes(func):
             if getattr(expected_type, '__origin__', None) is Union:
                 tmp_types = expected_type.__args__
                 if not any(is_instance(value, t) for t in tmp_types):
-                    types_expected = ', '.join(
-                        type_to_string(t) for t in tmp_types[:-1]
-                    ) + ' or ' + type_to_string(tmp_types[-1])
-
+                    types_expected = ', '.join(type_to_string(t) for t in tmp_types[:-1]) + ' or ' + type_to_string(tmp_types[-1])
             elif not isinstance(value, expected_type):
-                types_expected = expected_type.__name__
+                types_expected = type_to_string(expected_type)
 
             if types_expected:
                 raise TypeError(
                     f"Argument '{name}' has incorrect type. "
                     f"Expected {types_expected}, "
-                    f"got '{type_to_string(value)}'",
+                    f"got '{type_to_string(value)}'"
                 )
         return func(*args, **kwargs)
 
