8abcb46732f859dee0dfabd139b8e783bc6a2f57
bcncalling <142149038+bcncalling@users.noreply.github.com>
2024-02-07 15:53:57 +0530

    Update ytdlp.py

--- pytgcalls/ytdlp.py
+++ pytgcalls/ytdlp.py
@@ -16,7 +16,7 @@ class YtDlp:
 
     @staticmethod
     def is_valid(link: str) -> bool:
-        return YtDlp.YOUTUBE_REGX.match(link) is not None
+        return bool(YtDlp.YOUTUBE_REGX.match(link))
 
     @staticmethod
     async def extract(
@@ -24,19 +24,24 @@ class YtDlp:
         video_parameters: VideoParameters,
     ) -> Tuple[str, str]:
         try:
-            proc = await asyncio.create_subprocess_exec(
+            process = await asyncio.create_subprocess_exec(
                 'yt-dlp',
                 '-g',
                 '-f',
-                f'best[width<=?{video_parameters.width}]'
-                f'[height<=?{video_parameters.height}]',
+                f'best[width<=?{video_parameters.width}][height<=?{video_parameters.height}]',
                 link,
                 stdout=asyncio.subprocess.PIPE,
                 stderr=asyncio.subprocess.PIPE,
             )
-            stdout, stderr = await proc.communicate()
-            data = stdout.decode().split('\n')
-            data[1] = data[0] if not data[1] else data[1]
-            return data[0], data[1]
+            stdout, stderr = await process.communicate()
+            if stderr:
+                raise YtDlpError(stderr.decode())
+            data = stdout.decode().strip().split('\n')
+            if len(data) >= 2:
+                return data[0], data[1]
+            elif data:
+                return data[0], ''
+            else:
+                raise YtDlpError('No video URLs found')
         except FileNotFoundError:
             raise YtDlpError('yt-dlp is not installed on your system')
