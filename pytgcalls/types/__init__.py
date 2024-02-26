25dc0fb780874ca4ad330bf2f10df83f91cf7f3f
Laky64 <iraci.matteo@gmail.com>
2024-02-26 20:11:29 +0100

    EOL for PyTgCalls 0.9.7

--- pytgcalls/types/__init__.py
+++ pytgcalls/types/__init__.py
@@ -10,48 +10,20 @@ from .stream import MediaStream
 from .stream import StreamAudioEnded
 from .stream import StreamVideoEnded
 from .stream import VideoQuality
-from .stream.legacy import AudioImagePiped
-from .stream.legacy import AudioPiped
-from .stream.legacy import AudioVideoPiped
-from .stream.legacy import CaptureAudioDevice
-from .stream.legacy import CaptureAVDesktop
-from .stream.legacy import CaptureAVDeviceDesktop
-from .stream.legacy import CaptureVideoDesktop
-from .stream.legacy import VideoPiped
-from .stream.legacy.quality import HighQualityAudio
-from .stream.legacy.quality import HighQualityVideo
-from .stream.legacy.quality import LowQualityAudio
-from .stream.legacy.quality import LowQualityVideo
-from .stream.legacy.quality import MediumQualityAudio
-from .stream.legacy.quality import MediumQualityVideo
 from .update import Update
 
 __all__ = (
-    'AudioImagePiped',
-    'AudioPiped',
     'AudioQuality',
-    'AudioVideoPiped',
     'Browsers',
     'Cache',
-    'CaptureAVDesktop',
-    'CaptureAVDeviceDesktop',
-    'CaptureAudioDevice',
-    'CaptureVideoDesktop',
     'GroupCall',
     'GroupCallParticipant',
-    'HighQualityAudio',
-    'HighQualityVideo',
     'JoinedGroupCallParticipant',
     'LeftGroupCallParticipant',
-    'LowQualityAudio',
-    'LowQualityVideo',
     'MediaStream',
-    'MediumQualityAudio',
-    'MediumQualityVideo',
     'StreamAudioEnded',
     'StreamVideoEnded',
     'Update',
     'UpdatedGroupCallParticipant',
-    'VideoPiped',
     'VideoQuality',
 )
