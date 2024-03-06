0b7ebfe8d2656744ced1312e87d48244f701f1d2
Laky64 <iraci.matteo@gmail.com>
2024-03-06 03:24:09 +0100

    Updated Requirements

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'aiohttp>=3.9.3',
-        'ntgcalls>=1.1.3b6',
+        'ntgcalls>=1.1.3',
         'psutil',
         'screeninfo',
         'deprecation',
