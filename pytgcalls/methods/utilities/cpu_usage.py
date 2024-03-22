d815235d0bb9f837bb3afc85c759be5b521b873b
Laky64 <iraci.matteo@gmail.com>
2024-03-22 22:18:30 +0100

    Fixed wrong calling to cpu_usage method

--- pytgcalls/methods/utilities/cpu_usage.py
+++ pytgcalls/methods/utilities/cpu_usage.py
@@ -4,4 +4,4 @@ from ...scaffold import Scaffold
 class CpuUsage(Scaffold):
     @property
     async def cpu_usage(self) -> float:
-        return await self._binding.cpuUsage()
+        return await self._binding.cpu_usage()
