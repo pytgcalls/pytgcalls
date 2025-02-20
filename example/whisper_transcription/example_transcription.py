f3fd657d60adc9b238804242f0e58a985316aeef
doggyhaha <75123663+doggyhaha@users.noreply.github.com>
2025-02-20 23:23:22 +0100

    Make example more coherent with other examples

--- example/whisper_transcription/example_transcription.py
+++ example/whisper_transcription/example_transcription.py
@@ -7,18 +7,30 @@ from ai_model import AIModel
 AUDIO_QUALITY = AudioQuality.HIGH
 
 model = AIModel(AUDIO_QUALITY)
-app = Client("py-tgcalls", api_id=123456789, api_hash="abcdef12345")
+
+app = Client(
+    'py-tgcalls', 
+    api_id=12345, 
+    api_hash="0123456789abcdef0123456789abcdef",
+)
+
 call_py = PyTgCalls(app)
+chat_id = 1234567890
 call_py.start()
 call_py.record(
-    "username",
+    chat_id,
     RecordStream(
         True,
         AUDIO_QUALITY,
     ),
 )
 
-@call_py.on_update(filters.stream_frame(Direction.INCOMING, Device.MICROPHONE))
+@call_py.on_update(
+    filters.stream_frame(
+        Direction.INCOMING, 
+        Device.MICROPHONE,
+    ),
+)
 async def audio_data(_: PyTgCalls, update: StreamFrame):
     stt = model.transcribe(update.frame)
     if stt:
