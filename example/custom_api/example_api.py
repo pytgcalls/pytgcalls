96458fa901734fdef7cf357fb1d405fea36d84c0
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2025-03-09 11:36:08 +0530

    Update example_api.py

--- example/custom_api/example_api.py
+++ example/custom_api/example_api.py
@@ -21,23 +21,21 @@ ca = CustomApi()
 # with Webhooks or bots in different programming language
 @ca.on_update_custom_api()
 async def custom_api_request(request: dict):
-    print(request)
+    print('Received request:', request)
     return {
         'response': 'BYE',
     }
 
-
 @app.on_message(filters.regex('!test'))
 def test_handler(client: Client, message: Message):
-    print(
-        requests.post(
-            'http://localhost:24859/', json.dumps({
-                'answer': 'HI',
-            }),
-        ).json(),
-    )
-
+    response = requests.post(
+        'http://localhost:24859/',
+        json={'answer': 'HI'},
+    ).json()
+    print('Response from CustomApi:', response)
+    message.reply_text(f"CustomApi Response: {response}")
 
 app.start()
 ca.start()
 idle()
+ca.stop()
