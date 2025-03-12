f89e161b99f6c00a1ecf631aeb4c3552cce8a95a
Laky64 <iraci.matteo@gmail.com>
2025-03-12 01:57:20 +0100

    Fixed some pre-commit issues

--- pytgcalls/methods/calls/get_participants.py
+++ pytgcalls/methods/calls/get_participants.py
@@ -17,7 +17,7 @@ class GetParticipants(Scaffold):
         chat_id: Union[int, str],
     ) -> Optional[List[GroupCallParticipant]]:
         chat_id = await self.resolve_chat_id(chat_id)
-        if chat_id >= 0:
+        if chat_id >= 0:  # type: ignore
             raise UnsupportedMethod()
         return await self._app.get_group_call_participants(
             chat_id,
